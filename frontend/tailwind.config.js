f2c47fc26627bd2e8fb957302d9767fb539f6f70
KAI-77 <umbongshan2717@gmail.com>
2024-12-29 15:08:41 +0800

    removed uplaods folder

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "mfp-blue": "#0066EE",
+        "mfp-purple": "#5D3EBC",
+      },
+    },
+  },
+  plugins: [],
+};
