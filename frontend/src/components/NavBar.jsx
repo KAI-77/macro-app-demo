69ac11556a654a15cebeb8bb9485a4e8741c5b87
KAI-77 <umbongshan2717@gmail.com>
2024-12-29 16:14:06 +0800

    added navbar for frontend/ initial commmit

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <nav className="bg-white shadow-sm">
+            <div className="max-w-7xl mx-auto sm:px-6 lg: px-8">
+                <div className="flex justify-between h-16">
+                    <div className="flex-shrink-0 flex items-center">
+                        <Link to="/" className="text-blue-600 text-2xl font-bold">
+                            Fitness Hustle
+                        </Link>
+                    </div>
+                    <div className="flex items-center">
+                        <button className="bg-blue-500 text-white px-6 py-2 rounded-full hover: bg-blue-700 transition-colors">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
