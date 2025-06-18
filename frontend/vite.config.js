64a10edfa23fae7ccedf62d69933140f31b267b3
KAI-77 <umbongshan2717@gmail.com>
2025-06-18 11:04:13 +0800

    fixed: vite configuration for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
-      proxy:
-        process.env.NODE_ENV === "development"
-          ? {
-              "/api": "http://localhost:5000",
-              "/auth": "http://localhost:5000",
-            }
-          : undefined,
     },
+    proxy:
+      process.env.NODE_ENV === "development"
+        ? {
+            "/api": "http://localhost:5000",
+            "/auth": "http://localhost:5000",
+          }
+        : {
+            "/api": "https://vitascan-backend.onrender.com",
+            "/auth": "https://vitascan-backend.onrender.com",
+          },
   },
 });
